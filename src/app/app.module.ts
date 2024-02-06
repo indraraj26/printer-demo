b887f412835a2d8a1ffad4d1d1151ef3207d8008
Indraraj Yadav <indra26@Indrarajs-Mac-mini.local>
2024-02-06 19:46:31 +0530

    feat: add bluetooth serial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { BluetoothSerial } from '@awesome-cordova-plugins/bluetooth-serial/ngx';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    BluetoothSerial,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
